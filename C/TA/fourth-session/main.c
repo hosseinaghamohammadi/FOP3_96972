2f5b5943e87fee5a23a24e24400aae46fd50803c
HosseinAghamohammadi <hosein_aghamohammadi@yahoo.com>
2018-03-18 11:14:37 +0330

    second assignment done and released!

--- C/TA/fourth-session/main.c
+++ C/TA/fourth-session/main.c
@@ -1,17 +1,153 @@
 #include <stdio.h>
 int main(){
-    int input = 0;
+    int input = 0, i = 0;
+    int fallSeen = 0, raiseSeen = 0, raisingFirst = 0, fallingFirst = 0;
+    int no = 0;
+    int previous = 0;
     scanf("%d", &input);
-    int i, sum = 0, income = 0;
-    for (i = 0; i < input; i++) {
+    for(i = 0; i < input; i++){
+        int income = 0;
         scanf("%d", &income);
-        if (income < 0){
-            sum += (-income);
-        } else{
-            sum += income;
+        if (i == 0){
+            previous = income;
+            continue;
         }
+        if (previous < income && fallSeen == 0){
+            raiseSeen = 1;
+            raisingFirst = 1;
+        }
+        if (previous > income && raiseSeen == 0){
+            fallSeen = 1;
+            fallingFirst = 1;
+        }
+        if (fallingFirst && previous < income){
+            raiseSeen = 1;
+        }
+        if (raisingFirst && previous > income){
+            fallSeen = 1;
+        }
+        if ((fallingFirst && raiseSeen && previous > income) || (raisingFirst && fallSeen && previous < income)){
+//            printf(income);
+            no = 1;
+        }
+        if ((fallingFirst && raiseSeen && previous == income) || (raisingFirst && fallSeen && previous == income)){
+//            printf(income);
+            no = 1;
+        }
+        previous = income;
     }
-    printf("%d", sum);
+    if (no){
+        printf("Ishala next year!");
+        return 0;
+    }
+    printf("yes");
+//    int input = 0;
+//    scanf("%d", &input);
+//    if (input < 10){
+//        printf("%d", input);
+//        return 0;
+//    } else if (input <= (9 + 90 * 2)){
+//        input -= 9;
+//        int index = (input+1) / 2;
+//        int theNumberContaining_k_th_digit = 9 + index;
+//        switch (input % 2){
+//            case 0:
+//                printf("%d", theNumberContaining_k_th_digit % 10);
+//                return 0;
+//            case 1:
+//                printf("%d", theNumberContaining_k_th_digit / 10);
+//                return 0;
+//        }
+//    } else if (input <= (9 + 90 * 2 + 900 * 3)){
+//        input -= (9 + 2 * 90);
+//        int index = (input + 2) / 3;
+//        int theNumberContaining_k_th_digit = 99 + index;
+//        switch (input % 3){
+//            case 0:
+//                printf("%d", theNumberContaining_k_th_digit % 10);
+//                return 0;
+//            case 1:
+//                printf("%d", theNumberContaining_k_th_digit / 100);
+//                return 0;
+//            case 2:
+//                printf("%d", (theNumberContaining_k_th_digit / 10) % 10);
+//                return 0;
+//        }
+//    } else if (input <= (9 + 90 * 2 + 900 * 3 + 9000 * 4)){
+//        input -= (9 + 90 * 2 + 900 * 3);
+//        int index = (input + 3) / 4;
+//        int theNumberContaining_k_th_digit = 999 + index;
+//        switch (input % 4){
+//            case 0:
+//                printf("%d", theNumberContaining_k_th_digit % 10);
+//                return  0;
+//            case 1:
+//                printf("%d", theNumberContaining_k_th_digit / 1000);
+//                return 0;
+//            case 2:
+//                printf("%d", (theNumberContaining_k_th_digit / 100) % 10);
+//                return 0;
+//            case 3:
+//                printf("%d", (theNumberContaining_k_th_digit / 10) % 10);
+//        }
+//    } else if (input <= (9 + 90 * 2 + 900 * 3 + 9000 * 4 + 90000 * 5)){
+//        input -= (9 + 90 * 2 + 900 * 3 + 9000 * 4);
+//        int index = (input + 4) / 5;
+//        int theNumberContaining_k_th_digit = 9999 + index;
+//        switch (input % 5){
+//            case 1:
+//                printf("%d", theNumberContaining_k_th_digit / 10000);
+//                return 0;
+//            case 2:
+//                printf("%d", (theNumberContaining_k_th_digit / 1000) % 10);
+//                return 0;
+//            case 3:
+//                printf("%d", (theNumberContaining_k_th_digit / 100) % 10);
+//                return 0;
+//            case 4:
+//                printf("%d", (theNumberContaining_k_th_digit / 10) % 10);
+//                return 0;
+//            case 0:
+//                printf("%d", theNumberContaining_k_th_digit % 10);
+//                return 0;
+//        }
+//    } else if (input <= (9 + 90 * 2 + 900 * 3 + 9000 * 4 + 90000 * 5 + 900000 * 6)){
+//        input -= (9 + 90 * 2 + 900 * 3 + 9000 * 4 + 90000 * 5);
+//        int index = (input + 5) / 6;
+//        int theNumberContaining_k_th_digit = 99999 + index;
+//        switch (input % 6){
+//            case 1:
+//                printf("%d", theNumberContaining_k_th_digit / 100000);
+//                return 0;
+//            case 2:
+//                printf("%d", (theNumberContaining_k_th_digit / 10000) % 10);
+//                return 0;
+//            case 3:
+//                printf("%d", (theNumberContaining_k_th_digit / 1000) % 10);
+//                return 0;
+//            case 4:
+//                printf("%d", (theNumberContaining_k_th_digit / 100) % 10);
+//                return 0;
+//            case 5:
+//                printf("%d", (theNumberContaining_k_th_digit / 10)% 10);
+//                return 0;
+//            case 0:
+//                printf("%d", theNumberContaining_k_th_digit % 10);
+//                return 0;
+//        }
+//    }
+//    int input = 0;
+//    scanf("%d", &input);
+//    int i, sum = 0, income = 0;
+//    for (i = 0; i < input; i++) {
+//        scanf("%d", &income);
+//        if (income < 0){
+//            sum += (-income);
+//        } else{
+//            sum += income;
+//        }
+//    }
+//    printf("%d", sum);
 
 //    int input = 0;
 //    scanf("%d", &input);
